4553cbfbcad0da9bca5a04e44c6a9fd88a73b0ed
Chalant <Strikerklm96@gmail.com>
2021-03-17 00:27:15 -0500

    two linter errors

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,12 +3,14 @@ import './App.css';
 import io from "socket.io-client";
 
 console.log('Client loading in browser.')
-class App extends React.Component {
+class App extends React.Component
+{
 	// Initialize state
 	state = { passwords: [] }
 
 	// Fetch passwords after first mount
-	componentDidMount() {
+	componentDidMount()
+	{
 		//var socket = io();
 		const script = document.createElement('script'); // d
 
@@ -26,60 +28,63 @@ class App extends React.Component {
 		// });
 	}
 
-	getPasswords = () => {
+	getPasswords = () =>
+	{
 		// Get the passwords and store them in state
 		fetch('/api/passwords')
 			.then(res => res.json())
 			.then(passwords => this.setState({ passwords }));
 	}
 
-	render() {
+	render()
+	{
 		console.log('Client rendering in browser.s')
 		const { passwords } = this.state;
 
-		if (passwords.length > 0) 
-		{ 
+		if (passwords.length)
+		{
 
 		}
 
 		return (
 			<div className="App">
 				{/* Render the passwords if we have them */}
-				{passwords.length === 0 ? (
-					<div>
-						<h1>5 Passwords.</h1>
-						<ul className="passwords">
-							{/*
+				{passwords.length !== 0 ?
+					(
+						<div>
+							<h1>5 Passwords.</h1>
+							<ul className="passwords">
+								{/*
 								Generally it's bad to use "index" as a key.
 								It's ok for this example because there will always
 								be the same number of passwords, and they never
 								change positions in the array.
 							*/}
-							{passwords.map((password, index) =>
-								<li key={index}>
-									{password}
-								</li>
-							)}
-						</ul>
-						<button
-							className="more"
-							onClick={this.getPasswords}>
-							Get More
-            			</button>
-					</div>
-				) 
-				: 
+								{passwords.map((password, index) =>
+									<li key={index}>
+										{password}
+									</li>
+								)}
+							</ul>
+							<button
+								className="more"
+								onClick={this.getPasswords}>
+								Get More
+            				</button>
+						</div>
+					)
+					:
 					(
-					// Render a helpful message otherwise
+						// Render a helpful message otherwise
 						<div>
-						<h1>No passwords :(</h1>
-						<button
-							className="more"
-							onClick={this.getPasswords}>
-							Try Again?
-            			</button>
-					</div>
-				)}
+							<h1>No passwords :(</h1>
+							<button
+								className="more"
+								onClick={this.getPasswords}>
+								Try Again?
+            				</button>
+						</div>
+					)}
 			</div>
 		);
 	}
--- index.ts
+++ index.ts
@@ -1,64 +1,62 @@
-import * as exp from 'express';
-import * as http from 'http';
-import * as io from 'socket.io';
-import * as path from 'path';
-import * as pg from 'pg';
-import * as dotenv from 'dotenv';
+import * as exp from "express";
+import * as http from "http";
+import * as io from "socket.io";
+import * as path from "path";
+import * as pg from "pg";
+import * as dotenv from "dotenv";
+/// eslint-disable-next-line @typescript-eslint/no-explicit-any
 declare const process: any;
 declare const __dirname: string;
 
 dotenv.config();
 
-import express from 'express';
+import express from "express";
 const expWrap = express();
 const httpServer = http.createServer(expWrap);
 const ioWrap = new io.Server(httpServer);
 
-
-
-
-ioWrap.on('connection', (socket: io.Socket) => {
+ioWrap.on("connection", (socket: io.Socket) =>
+{
 	console.log(`a user connected`);
-	socket.on('disconnect', () => {
-		console.log('user disconnected');
+	socket.on("disconnect", () =>
+	{
+		console.log("user disconnected");
 	});
 });
 
-
-
-
 // pools will use environment variables
 // for connection information
-const pool = new pg.Pool({
-	connectionString: process.env.DATABASE_URL,
-	ssl: process.env.DATABASE_SKIPSSL === "true" ? undefined : { rejectUnauthorized: false },
-});
-
-
+const pool = new pg.Pool(
+	{
+		connectionString: process.env.DATABASE_URL,
+		ssl: process.env.DATABASE_SKIPSSL === "true" ? undefined : { rejectUnauthorized: false },
+	}
+);
 
 // Serve static files from the React app
-expWrap.use(express.static(path.join(__dirname, 'client/build')));
+expWrap.use(express.static(path.join(__dirname, "client/build")));
 
 // Put all API endpoints under '/api'
-expWrap.get('/api/passwords', async (req, res: exp.Response) => {
+expWrap.get("/api/passwords", async (req, res: exp.Response) =>
+{
 	const data = [];
 
-	const databaseRes = await pool.query('SELECT * FROM horses;');//, (err, res) => 
+	const databaseRes = await pool.query("SELECT * FROM horses;"); //, (err, res) =>
 
-	for (const row of databaseRes.rows) {
-		if (databaseRes.rows.length)
-			data.push(JSON.stringify(row));
+	for (const row of databaseRes.rows)
+	{
+		data.push(JSON.stringify(row));
 	}
 	res.json(data);
-
 
 	console.log(`Sent data back to clients.ss`);
 });
 
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
-expWrap.get('*', (req: exp.Request, res: exp.Response) => {
-	res.sendFile(path.join(__dirname + '/client/build/index.html'));
+expWrap.get("*", (req: exp.Request, res: exp.Response) =>
+{
+	res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
 const port = process.env.PORT;
